3d9e35976bc98d00729c90bc5f0646dd0644566a
roxfordlc <roxfordlc@gmail.com>
2020-05-12 01:48:34 -0500

    adding questions

---
+++ Week3GroupProject/Models/GameRating.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week3GroupProject.Models
+{
+    public class GameRating
+    {
+        public int ID { get; set; }
+        public Game game { get; set; }
+        public List<ESRBRating> ratings { get; set; }
+
+        public GameRating()
+        {
+            game = new Game();
+            ratings = new List<ESRBRating>();
+        }
+    }
+}
